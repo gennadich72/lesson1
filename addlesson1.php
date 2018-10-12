1a1b9479ca795a066c89c8477d9a8972b0d9cdc4
Andrey <at-arb@yandex.ru>
2018-10-12 22:25:35 +0400

    Дополнительное задание к уроку №1

---
+++ addlesson1.php
@@ -0,0 +1,49 @@
+<?php
+/**
+дополнительное задание к уроку
+ */
+//Объявляем переменные
+$UserInt = $_GET['UserInt']; //Значение введенное пользователем
+$a = 1;
+$b = 1;
+$UserMessage = NULL;
+//$c = NULL;
+begin:
+if (isset($UserInt))
+{
+    if ($a > $UserInt)
+        {
+           $UserMessage =  "Число $UserInt НЕ входит в числовой ряд";
+        }
+        elseif ($a == $UserInt)
+            {
+                $UserMessage = "Число $UserInt входит в числовой ряд";
+            }
+            else
+                {
+                $c = $a;
+                $a = $a + $b;
+                $b = $c;
+                goto begin;
+                }
+}
+?>
+
+<!--HTML код -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Lesson1</title>
+</head>
+<body>
+<form action="addlesson1.php" method="get">
+    <p><input type="text" name="UserInt"></p>
+    <p><input type="submit"></p>
+    <p><?php echo $UserMessage?></p>
+
+</form>
+
+</body>
+</html>
